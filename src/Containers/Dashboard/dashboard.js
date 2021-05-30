efc1f0e0cd02a2ebf6931f210b3e29a0e88b2445
Dixith Maram <dixithmaram@Dixiths-MacBook-Air.local>
2021-05-30 18:08:34 -0500

    add jobs

--- src/Containers/Dashboard/dashboard.js
+++ src/Containers/Dashboard/dashboard.js
@@ -12,12 +12,31 @@ componentWillMount(){
 
     render() {
         const jobItems = this.props.jobs.map(job => (
-            <div key={job.id}>
-              <h3>{job.title}</h3>
-              <p>{job.type}</p>
-              <p>{job.company}</p>
-              <p>{job.location}</p>
-            </div>
+          <Container style={{}}>
+          <Grid container spacing={4} direction="row" justify="flex-start" alignItems="flex-start">
+                  <Grid item xs={12} sm={6} md={4} key={index}>
+                      <Card className={classes.root} >
+                      <CardContent>
+                          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                          {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
+                          {user.id}
+                          </Typography> */}
+                          <Typography variant="h5" component="h2">
+                          {job.company}
+                          </Typography>
+                          <Typography className={classes.pos} color="textSecondary">
+                          {job.location}
+                          </Typography>
+                          <Typography variant="body2" component="p">
+                          {job.type}
+                          <br />
+                          {'"a benevolent smile"'}
+                          </Typography>
+                      </CardContent>
+                  </Card>
+                  </Grid>
+          </Grid>
+      </Container>
           ));
           return (
             <div>
