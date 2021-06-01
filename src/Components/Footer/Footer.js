a33eef34df734b7e2e24a8c8eeaf70e248794e7f
Dixith Maram <dixithmaram@Dixiths-MacBook-Air.local>
2021-06-01 17:01:49 -0500

    add footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+import styled from "styled-components";
+import Typography from '@material-ui/core/Typography';
+import Button from "@material-ui/core/Button"
+const FooterContainer = styled.div`
+  text-align: center;
+//   position: absolute;
+
+  bottom: 0;
+  width: 100% !important;
+  height: 100px !important ;
+  background: white;
+`;
+
+class Footer extends Component {
+  render() {
+    return (
+      <FooterContainer>
+        <div style={{display: "flex", justifyContent:"flex-start"}}>
+        <div>
+        <Typography component="h5" variant="h5">
+            So Digital lnc.
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            sodigital.co
+          </Typography>
+          </div>
+        {/* <div className="site-button" > */}
+        <div style={{display: "flex", justifyContent:"space-between"}}>
+      <Button  className="search-button" variant="contained" color="primary" style={{fontWeight: "bolder", margin:"10px"}}>
+        Apply Now
+    </Button>
+    </div>
+      {/* </div> */}
+        </div>
+      </FooterContainer>
+    );
+  }
+}
+
+export default Footer;
