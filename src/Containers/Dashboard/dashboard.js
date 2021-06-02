59d2569f8a5a57ca1ffa9e2fa30da5fa7189480f
Dixith Maram <dixithmaram@Dixiths-MacBook-Air.local>
2021-06-02 16:44:55 -0500

    remove unused code

--- src/Containers/Dashboard/dashboard.js
+++ src/Containers/Dashboard/dashboard.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { fetchJobs, fetchCurrentLocationJobs, fetchTermJobs, fetchGivenLocationJobs, fetchFullTimeJobs , fetchFilterJobs}  from '../../library/store/actions/jobActions';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 // import Typography from '@material-ui/core/Typography';
 // import Card from '@material-ui/core/Card';
 // import CardContent from '@material-ui/core/CardContent';
@@ -105,25 +105,25 @@ const mapStateToProps = state => ({
 })
 
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  }
-  ,
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-    borderRadius:"10%",
-    flex:"1"
-  },
-  pos: {
-    marginBottom: 12,
-  }
-});
+// const useStyles = makeStyles({
+//   root: {
+//     minWidth: 275,
+//   }
+//   ,
+//   bullet: {
+//     display: 'inline-block',
+//     margin: '0 2px',
+//     transform: 'scale(0.8)',
+//   },
+//   title: {
+//     fontSize: 14,
+//     borderRadius:"10%",
+//     flex:"1"
+//   },
+//   pos: {
+//     marginBottom: 12,
+//   }
+// });
 
 
 export default connect(mapStateToProps, { fetchJobs, fetchCurrentLocationJobs, fetchTermJobs, fetchGivenLocationJobs, fetchFullTimeJobs,
