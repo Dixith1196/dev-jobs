b255480df40165bcec609bc437f001864e74b9f8
Dixith Maram <dixithmaram@Dixiths-MacBook-Air.local>
2021-06-02 17:29:52 -0500

    remove typography

--- src/Containers/Dashboard/dashboard.js
+++ src/Containers/Dashboard/dashboard.js
@@ -6,7 +6,7 @@ import { fetchJobs, fetchCurrentLocationJobs, fetchTermJobs, fetchGivenLocationJ
 // import Typography from '@material-ui/core/Typography';
 // import Card from '@material-ui/core/Card';
 // import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import "./dashboard.css"
 // import { Container } from 'react-bootstrap';
 // import Grid from '@material-ui/core/Grid';
